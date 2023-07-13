e7b70f3f9efe9e63c2f1501078eee2976949875a
BreElle Wells <breellejordyn@gmail.com>
2023-07-13 17:28:56 -0600

    build a functioning to-do list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskButton = document.getElementById("create-task-button")
+  taskButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    createTask();
+    document.getElementById("create-task-form").reset();
+  })
 });
+
+function createTask() {
+  const input = document.getElementById("new-task-description");
+  const bullet = document.createElement("li")
+  bullet.innerText = input.value
+  document.getElementById("tasks").appendChild(bullet)
+}
